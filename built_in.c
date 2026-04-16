5597e45a3b970ba01cb5602ecbb67d146eebdab1
PvPatelexe <79430884+PvPatelexe@users.noreply.github.com>
2026-04-15 22:11:17 -0400

    spaces

--- built_in.c
+++ built_in.c
@@ -9,7 +9,6 @@ char *search_dirs[] = {"/usr/local/bin", "/usr/bin", "/bin", NULL};
 
 int find_program(char *name, char *path) {
     int i;
-
     if (has_slash(name)) {    //check directly if command has '/'
         if (access(name, X_OK) == 0) {
             strcpy(path, name);
@@ -17,21 +16,17 @@ int find_program(char *name, char *path) {
         }
         return 0;
     }
-
     if (is_builtin(name)) {    //not external
         return 0;
     }
-
     for (i = 0; search_dirs[i] != NULL; i++) {
         snprintf(path, PATH_MAX, "%s/%s", search_dirs[i], name);
         if (access(path, X_OK) == 0) {
             return 1;
         }
     }
-
     return 0;
 }
-
 int builtin_pwd(void) { 
     char cwd[PATH_MAX];
 
@@ -39,11 +34,9 @@ int builtin_pwd(void) {
         perror("pwd");      //failure
         return 1;
     }
-
     printf("%s\n", cwd); //prints current directory on success
     return 0;
 }
-
 int builtin_cd(char *argv[]) {
     char *dir;
     //handling cd arguments
@@ -61,12 +54,10 @@ int builtin_cd(char *argv[]) {
     else {
         dir = argv[1];
     }
-
     if (chdir(dir) != 0) {
         perror("cd");
         return 1;
     }
-
     return 0;
 }
 
@@ -75,16 +66,12 @@ int builtin_which(char *argv[]) {
     char path[PATH_MAX];
 
     if (argv[1] == NULL || argv[2] != NULL || is_builtin(argv[1])) return 1; //checking logic
-
-
     if (find_program(argv[1], path)) {
         printf("%s\n", path);   //print if found
         return 0;
     }
-
     return 1;
 }
-
 int run_builtin(char *argv[]) {
     if (strcmp(argv[0], "cd") == 0) return builtin_cd(argv);
     if (strcmp(argv[0], "pwd") == 0) return builtin_pwd();
